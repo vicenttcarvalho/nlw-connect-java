cef747ae227a5ed0c3f1241b5b635ced9e43c2f6
vicenttcarvalho <vicentecleyton@gmail.com>
2025-02-28 13:19:57 -0300

    feat: creating EventRepo

---
+++ src/main/java/br/com/nlw/events/repo/EventRepo.java
@@ -0,0 +1,8 @@
+package br.com.nlw.events.repo;
+
+import br.com.nlw.events.model.Event;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepo extends CrudRepository<Event, Integer> {
+    public Event findByPrettyName(String prettyName);
+}
