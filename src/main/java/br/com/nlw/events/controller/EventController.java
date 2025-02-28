c8c61b901dd8f0153e87bbd511a3d375f632fa01
vicenttcarvalho <vicentecleyton@gmail.com>
2025-02-28 13:39:13 -0300

    feat: Creating EventController

---
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -0,0 +1,30 @@
+package br.com.nlw.events.controller;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @PostMapping("/events")
+    public Event addNewEvent(@RequestBody Event newEvent) {
+        return service.addNewEvent(newEvent);
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents(){
+        return service.getAllEvents();
+    }
+
+
+}
